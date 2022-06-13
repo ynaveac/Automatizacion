022e78b5e28d083fc2c3d392dab159ac47a607d4
ynaveac <yencar1982@gmail.com>
2022-06-13 00:39:53 -0400

    rama master
    subir rama master

---
+++ Login/src/main/java/com/example/demo/models/service/IUsuarioService.java
@@ -0,0 +1,10 @@
+package com.example.demo.models.service;
+
+import com.example.demo.models.entity.Usuario;
+
+public interface IUsuarioService {
+
+	public Usuario findByUsername(String username);
+	public Usuario registrar(Usuario u);
+	
+}
