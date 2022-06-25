71fd95c20b840ed18e6e29bcc2c5a4a9c00b0e0d
ynaveac <yencar1982@gmail.com>
2022-06-25 01:14:33 -0400

    update

--- src/main/java/com/example/demo/LoginApplication.java
+++ src/main/java/com/example/demo/LoginApplication.java
@@ -2,10 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LoginApplication {
+public class LoginApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LoginApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LoginApplication.class, args);
 	}
